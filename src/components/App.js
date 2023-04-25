d6e544a01c1287c5816a0d4f034a0d3169fc9845
Livya Kendi Kiruja <livyakendikiruja@gmail.com>
2023-04-25 20:50:51 +0300

    Routes in React

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import { directors } from "../data";
 
-function App() {
-  return <div>{/*{code here}*/}</div>;
+function Directors() {
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    <div>
+      Name: {directors[0].name}
+      {<br />}Movies: 
+      <ul>
+        <li>{directors[0].movies[0]}</li>
+        <li>{directors[0].movies[1]}</li>
+        <li>{directors[0].movies[2]}</li>
+      </ul>
+    </div>
+    <div>
+      Name: {directors[1].name}
+      {<br />}Movies: 
+      <ul>
+        <li>{directors[1].movies[0]}</li>
+        <li>{directors[1].movies[1]}</li>
+        <li>{directors[1].movies[2]}</li>
+      </ul>
+    </div>
+    <div>
+      Name: {directors[2].name}
+      {<br />}Movies: 
+      <ul>
+        <li>{directors[2].movies[0]}</li>
+        <li>{directors[2].movies[1]}</li>
+        <li>{directors[2].movies[2]}</li>
+      </ul>
+    </div>
+  </div>
+  );
 }
 
-export default App;
+export default Directors;
